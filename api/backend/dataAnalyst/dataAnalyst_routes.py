744c322a5739da98fff4f9d9a4c893ff7cdf5b79
Justin Iverson <justiniver@icloud.com>
2024-08-13 12:52:10 -0400

    Update dataAnalyst_routes.py

--- api/backend/dataAnalyst/dataAnalyst_routes.py
+++ api/backend/dataAnalyst/dataAnalyst_routes.py
@@ -6,32 +6,24 @@ from flask import Blueprint, jsonify, make_response
 import json
 from backend.db_connection import db
 
+from flask import Blueprint, request, jsonify, make_response, current_app
+import json
+from backend.db_connection import db
+from backend.ml_models.model01 import predict
+
 dataAnalyst = Blueprint('dataAnalyst', __name__)
 
 @dataAnalyst.route('/dataAnalyst', methods=['GET'])
 def get_all_dataAnalyst():
+    
     cursor = db.get_db().cursor()
     the_query = """
     SELECT * 
     FROM employees
     """
     cursor.execute(the_query)
-    rows = cursor.fetchall()
-
-    # Get the column names from the cursor description
-    column_names = [desc[0] for desc in cursor.description]
-
-    # Convert the rows to a list of dictionaries
-    theData = [dict(zip(column_names, row)) for row in rows]
-
-    # Convert the data to JSON format
-    data_json = json.dumps(theData)
-    
-    # Create a response object
-    the_response = make_response(data_json)
+    theData = cursor.fetchall()
+    the_response = make_response(theData)
     the_response.status_code = 200
     the_response.mimetype = 'application/json'
-
     return the_response
-
-    
